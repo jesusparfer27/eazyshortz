5f602e2d61d587013740ed3dce7afcda736f527a
jesusparfer27 <jesusparfer@alumnos.cei.es>
2025-02-28 13:14:36 +0100

    commit

--- front/src/lib/Routes.jsx
+++ front/src/lib/Routes.jsx
@@ -1,22 +1,19 @@
-// routes.js
-
 import { createBrowserRouter } from "react-router-dom";
 import App from '../App';
 import { ErrorPage } from "@/pages/ErrorPage";
 import { ShortenPage } from "@/pages/ShortenPage";
 import { RedirectPage } from "@/pages/RedirectPage";
 
-// Crear las rutas
 const router = createBrowserRouter([
     {
-        path: '/', // Ruta raíz que carga la aplicación principal
+        path: '/', 
         element: <App />,
-        errorElement: <ErrorPage />, // Página de error por defecto
+        errorElement: <ErrorPage />,
         children: [
-            { index: true, element: <ShortenPage /> }, // Página principal
-            { path: '/:short_code', element:<RedirectPage /> }, // Página de productos
+            { index: true, element: <ShortenPage /> }
         ]
-    }
+    },
+    { path: '/:short_code', element: <RedirectPage /> } // Ahora está al mismo nivel que "/"
 ]);
 
 export default router;
